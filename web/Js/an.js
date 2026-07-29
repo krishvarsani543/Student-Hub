9d20421a137ffd15a7786a05e20b1de3fec19aed
krish <krishvarsani543@gmail.com>
2026-07-29 20:20:51 +0530

    update changes

---
+++ web/Js/an.js
@@ -0,0 +1,15 @@
+str="Hello from JavaScript"
+        function myfunction(a,b){
+            c=a+b
+            // return c
+            alert(c);
+        }
+        onload= myfunction(10,20)
+        // alert(myfunction(10,20))
+        var str=5
+        document.write("Welcome: " + str + "<br>")
+        document.write("Welcome: " + typeof(str))
+        if(true){
+            var x=10
+        }
+        document.write("Welcome: " + x + "<br>")
